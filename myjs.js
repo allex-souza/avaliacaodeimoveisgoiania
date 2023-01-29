7e4165f861a21d773fde97054a360f18c3326353
Alex Souza <59103542+allex-souza@users.noreply.github.com>
2023-01-29 14:39:41 -0300

    Vai que é tua
    Vai que é tua 10x

--- myjs.js
+++ myjs.js
@@ -1,3 +1,4 @@
 function openWpp(){
-    window.open("https://api.whatsapp.com/send?phone=5562999719118&text=Ol%C3%A1!%20Mauro,%20gostaria%20de%20saber%20mais%20sobre%20os%20seus%20servi%C3%A7os .")
+    /* window.open("https://api.whatsapp.com/send?phone=556299719118&text=*Ol%C3%A1!*%20Mauro,%20gostaria%20de%20saber%20mais%20sobre%20os%20seus%20servi%C3%A7os.") */
+    alert("Ops! este é apenas um protótipo.")
 }
